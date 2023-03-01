8152002713e0cb1ec81913541091821b2a4d5fcf
Rd <rd4ramadevi@gmail.com>
2023-03-01 14:54:10 +0530

    Custom PaintView Added

--- app/src/main/java/com/rd/scribbler/MainActivity.kt
+++ app/src/main/java/com/rd/scribbler/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.rd.scribbler
 
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //global declaration
+    companion object{
+        var path = Path()
+        var paintBrush = Paint()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
+        val eraser = findViewById<ImageButton>(R.id.whiteColor)
+
+        redBtn.setOnClickListener {
+            Toast.makeText(this, "Red clicked", Toast.LENGTH_LONG).show()
+        }
+
+        blueBtn.setOnClickListener {
+
+        }
+
+        blackBtn.setOnClickListener {
+
+        }
+
+        eraser.setOnClickListener {
+
+        }
     }
 }
